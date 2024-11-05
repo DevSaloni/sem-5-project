69d228e3923c269636a33356c18631a225d7a52b
DevSaloni <168973005+DevSaloni@users.noreply.github.com>
2024-11-05 18:34:43 +0530

    new added

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
 
 const express = require("express");
+const mongoose = require("mongoose");
+
+
+const maidRoutes = require('./routes/maidroute');
+const Maid = require("./models/maid")
+
 
 const app = express();
-
 const PORT = 2004;
 
-app.listen(PORT, () => console.log(`server started at ${PORT}`));
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.get('/footer', (req, res) => {
+    res.sendFile(__dirname + '/public/footer.html');
+  });
+  
+// Database Connection
+mongoose.connect("mongodb://127.0.0.1:27017/maidDb")
+.then(() => console.log("Connected to MongoDB successfully..."))
+.catch(err => console.log("Error connecting to MongoDB:", err.message));
+
+// Routes
+app.use('/', maidRoutes);
+
+app.listen(PORT, () => console.log(`Server started at http://localhost:${PORT}`));
 
 
